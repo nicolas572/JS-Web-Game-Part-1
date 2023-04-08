7dcc6487b480fa2e7865c19b734a166a04000057
Nicolas Nueca <nicolasnueca@nicolass-air.lan>
2023-04-08 10:30:51 -0700

    added background images

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function tile(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
 function newImage(url, left, bottom) {
     let object = document.createElement('img')
     object.src = url
@@ -15,6 +23,13 @@ function newItem(url, left, bottom) {
         object.remove()
     })
 }
+
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
 
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/tree.png', 200, 300)
